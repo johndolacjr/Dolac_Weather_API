8ca4f00a47b2ae4869d2b08e5d332eec6a58f825
johndolac <johndolacjr@gmail.com>
2020-10-24 12:04:20 -0600

    working icons

--- java.js
+++ java.js
@@ -4,6 +4,8 @@ $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 var button = document.querySelector('.searchCity');
 var inputValue = document.querySelector('.inputValue'); 
 var cityName = document.querySelector('.cityName');
+var todayIcon = document.querySelector('.today-icon');
+var tomorrowIcon = document.querySelector('.tomorrow-icon')
 var temp = document.querySelector('.temp');
 var humidity = document.querySelector('.humidity');
 var windSpeed = document.querySelector('.windSpeed');
@@ -17,19 +19,31 @@ button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid='+apiKey) 
     .then(response => response.json())
     .then(data => {
-        var cityNameValue = data.name;
+        var currentDate = new Date(data.dt*1000);
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth();
+        var year = currentDate.getFullYear();
+        var cityNameValue = data.name + " (" + month + "/" + day + "/" + year +") ";
         var tempValue = data.main.temp;
         var humidityValue = data.main.humidity;
         var windSpeedValue = data.wind.speed;
         var iconValue = data.weather[0].icon;
+       
 
-        cityName.innerHTML = `City: ${cityNameValue}`;
+        cityName.innerHTML = `${cityNameValue}`;
         temp.innerHTML = `Temperature: ${tempValue} F`;
         humidity.innerHTML = `Humidity: ${humidityValue}%`;
         windSpeed.innerHTML = `Wind Speed: ${windSpeedValue} MPH`;
         
         getUvIndex(data.coord.lat, data.coord.lon);
         getForecast(inputValue.value);
+
+
+        var iconImg = "https://openweathermap.org/img/wn/" + iconValue + ".png";
+        todayIcon.setAttribute("src", iconImg);
+        tomorrowIcon.setAttribute("src", iconImg);
+        
+
     });
 });
 
@@ -44,15 +58,13 @@ function getUvIndex (lat,lon){
     });
 
 };
-//Create the 5 day forcast --> can I use my widget here?
+//Create the 5 day forcast
 function getForecast(city){
-    fetch ("https://api.openweathermap.org/data/2.5/forecast?q=" +
-    city +
-    "&appid="+apiKey+"&units=imperial")
+    fetch ("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial")
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-     for (i = 0; i < data.list.length; i += 8)
+        console.log(data);
+     for (i = 0; i < data.list.length; i += 8);
         // create 5 cards to populate the 5 day forcast (temp and humidity, icon and date)no UV index for 5 days)
     
     });
@@ -60,7 +72,6 @@ function getForecast(city){
 
 // goes into original forecast and 5 days 
 //Create image tag - 
-// "http://openweathermap.org/img/w/" + icon + ".png" 
 
 
 //Create local storage list inside of a column container 
